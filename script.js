2e232914fb2269388d9e7cb6c8805835f2eab3ec
causeway-rms <118050291+causeway-rms@users.noreply.github.com>
2023-05-31 13:42:39 +0530

    Add clear function to the CLR button

--- script.js
+++ script.js
@@ -18,11 +18,11 @@ function divide(a,b) {
 
 // The variables
 
-let firstNumber;
-let secondNumber;
-let operator;
+let firstNumber = '';
+let secondNumber = '';
+let operator = '';
 
-// Basic operation
+// Calls the relevant math function with required parameters
 
 function operate(op,x,y) {
     switch(op) {
@@ -43,11 +43,12 @@ function operate(op,x,y) {
     }
 }
 
-// Displaying things
+// Displaying things when buttons are clicked
 
 const displayValue = document.querySelector('.display-text');
 
 const displayNumbers = document.querySelectorAll('.numbers');
+
 for(let i = 0; i < displayNumbers.length; i++) {
     displayNumbers[i].addEventListener('click', function() {
         displayValue.value = displayValue.value + displayNumbers[i].textContent;
@@ -57,12 +58,13 @@ for(let i = 0; i < displayNumbers.length; i++) {
 // Value storage and triggering function
 
 const selectOperator = document.querySelectorAll('.operators');
+
 for(let i =0; i < selectOperator.length; i++) {
     selectOperator[i].addEventListener('click', function() {
-        if (firstNumber == undefined) {
+        if (firstNumber == '') {
             firstNumber = parseInt(displayValue.value);
             operator = selectOperator[i].id;
-            displayValue.value = "";
+            displayValue.value = '';
         }
         else {
             secondNumber = parseInt(displayValue.value);
@@ -70,3 +72,13 @@ for(let i =0; i < selectOperator.length; i++) {
         }
     });
 };
+
+// refreshes when clear button is pressed
+
+const clearsAll = document.getElementById('clear');
+clearsAll.addEventListener('click', function() {
+    firstNumber = '';
+    secondNumber = '';
+    operator = '';
+    displayValue.value = '';
+})
