995581a2c2cd488662ad9fec3dff80c06d593b95
causeway-rms <118050291+causeway-rms@users.noreply.github.com>
2023-05-31 05:52:27 +0530

    Add operate function that calls appropriate math functions

--- script.js
+++ script.js
@@ -1,16 +1,39 @@
 function add(a,b) {
-    return (a+b);
+    console.log(a+b);
 }
 
 function subtract(a,b) {
-    return (a-b);
+    console.log(a-b);
 }
 
 function multiply(a,b) {
-    return (a*b);
+    console.log(a*b);
 }
 
 function divide(a,b) {
-    return (a/b);
+    console.log(a/b);
+}
+
+let firstNumber;
+let secondNumber;
+let operator;
+
+function operate(op,x,y) {
+    switch(op) {
+        case 'add':
+            add(x,y);
+            break;
+        case 'subtract':
+            subtract(x,y);
+            break;
+        case 'multiply':
+            multiply(x,y);
+            break;
+        case 'divide':
+            divide(x,y);
+            break;
+        default:
+            console.log('Invalid input');
+    }
 }
 
